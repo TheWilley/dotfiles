908569545d7214df396358a622f439aed94388f4
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-02-01 11:31:04 +0100

    fixed error where certain flathub packages would not isntall and where the user would be asked it it wanted to install packages all the time

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -63,11 +63,13 @@ install_i3() {
 }
 
 install_flatpak_packages() {
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
     flatpak_dependencies=("md.obsidian.Obsidian" "com.github.tchx84.Flatseal" "org.shotcut.Shotcut" "org.blender.Blender")
     
     for dependency in "${flatpak_dependencies[@]}"; do
         if ! command_exists "$dependency"; then
-            flatpak install flathub "$dependency"
+            flatpak install flathub -y "$dependency"
         fi
     done
 }
