da4febe1e4f4c37dc9068a9635e19997500841f8
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-02-02 18:05:31 +0100

    the correct deps are now installed for i3block-color

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -75,13 +75,13 @@ install_i3() {
     # Install i3lock-color
     i3lock_dependencies=("autoconf" "gcc" "make" "pkg-config" "libpam0g-dev" "libcairo2-dev" "libfontconfig1-dev" "libxcb-composite0-dev" "libev-dev" "libx11-xcb-dev" "libxcb-xkb-dev" "libxcb-xinerama0-dev" "libxcb-randr0-dev" "libxcb-image0-dev" "libxcb-util-dev" "libxcb-xrm-dev" "libxkbcommon-dev" "libxkbcommon-x11-dev" "libjpeg-dev")
     
-    install_apt_dependencies "${dependencies[@]}"
+    install_apt_dependencies "${i3lock_dependencies[@]}"
     
     cd /tmp
     git clone https://github.com/Raymo111/i3lock-color.git
     cd i3lock-color
     ./install-i3lock-color.sh
-    mv ./build/i3lock $HOME/.local/bin/i3lock
+    mv build/i3lock $HOME/.local/bin/i3lock
     rm -rf /tmp/i3lock-color
 }
 
