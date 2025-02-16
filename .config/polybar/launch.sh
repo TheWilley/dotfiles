89560f2181ea78d20eb1e339c14a16d08d840cdd
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-02-16 17:10:47 +0100

    now uses i3 internally to handle workspaces

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # Determine bar name based on primary status
+    if xrandr --query | grep "^$m connected primary"; then
+      bar_name="top-primary"
+    else
+      bar_name="top-secondary"
+    fi
+
+    MONITOR="$m" polybar --reload "$bar_name" &
+  done
+else
+  polybar --reload top-primary &
+fi
 
 echo "Bars launched..."
